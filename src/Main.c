4fe4c2d8d85a72f3e014e9a572da2e12ef7e7adf
Brisset Joachim <joachim.brisset@eisti.fr>
2020-05-18 15:48:45 +0200

    Adding new struct for the game dev

--- src/Main.c
+++ src/Main.c
@@ -2,6 +2,20 @@
 #define Main
 
 #include <stdio.h>
+/**
+ * @struct Box
+ * @brief represente a box in the game
+ */
+typedef struct {
+    int value;
+} Box;
+
+#define BOARD_LENGHT 4
+/**
+ * @def board
+ * @brief use struct box if we want to add feature
+ */
+Box* board[BOARD_LENGHT][BOARD_LENGHT] = NULL;
 
 int main(int argc, char** argv) {
 	printf("Hello Wolrd ! \n");
