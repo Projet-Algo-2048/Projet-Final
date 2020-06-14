378ffc5725f81b5bc7c2326713cff5dcd0fd33b9
Brisset Joachim <joachim.brisset@eisti.fr>
2020-06-14 15:38:32 +0200

    Fix blinking pause menu

--- src/pause.c
+++ src/pause.c
@@ -55,101 +55,85 @@ int pauseMenu (int *red, int *green, int *blue, TTF_Font *font, SDL_Renderer *re
     SDL_bool pauseRunning = SDL_TRUE;
     int mouse_x, mouse_y = 0;
 
-    while(pauseRunning)
-        {
-            while(SDL_PollEvent(&pauseEvent))
-                {
-                    switch(pauseEvent.type)
-                        {
-                            case SDL_QUIT:
-                                pauseRunning = SDL_FALSE;
-                                return 0;
-                            break;
+    while(pauseRunning)  {
+        while(SDL_PollEvent(&pauseEvent)) {
+            switch(pauseEvent.type) {
+                case SDL_QUIT:
+                    pauseRunning = SDL_FALSE;
+                    return 0;
+                break;
 
-                            case SDL_KEYDOWN:
-                                switch(pauseEvent.key.keysym.sym)
-                                    {
-                                        case SDLK_ESCAPE:
-                                            pauseRunning = SDL_FALSE;
-                                            return 1;
-                                        break;
-                                    }
-                            break;
+                case SDL_KEYDOWN:
+                     switch(pauseEvent.key.keysym.sym) {
+                        case SDLK_ESCAPE:
+                             pauseRunning = SDL_FALSE;
+                              return 1;
+                        break;
+                     }
+                break;
 
 
-                            case SDL_MOUSEMOTION:
-                                mouse_x = pauseEvent.motion.x;
-                                mouse_y = pauseEvent.motion.y;
-                                //switch button color depending of the cursor location
-                                //case resume
-                                if ((mouse_x < resumeRect.x + resumeRect.w)&&
-                                    (mouse_x > resumeRect.x) &&
-                                    (mouse_y < resumeRect.y + resumeRect.h)&&
-                                    (mouse_y > resumeRect.y))
-                                    {
-                                        resume = TTF_RenderText_Solid(font, getTranslatedText("paMenu.re"), rouge);
-                                        resumeTexture = SDL_CreateTextureFromSurface(renderer, resume);
-                                    }
-                                    else
-                                    {
-                                        resume = TTF_RenderText_Solid(font, getTranslatedText("paMenu.re"), white);
-                                        resumeTexture = SDL_CreateTextureFromSurface(renderer, resume);
-                                    }
-                                //case home
-                                if ((mouse_x < homeRect.x + homeRect.w)&&
-                                    (mouse_x > homeRect.x) &&
-                                    (mouse_y < homeRect.y + homeRect.h)&&
-                                    (mouse_y > homeRect.y))
-                                    {
-                                        homeSurface = TTF_RenderText_Solid(font, getTranslatedText("paMenu.ho"), rouge);
-                                        homeTexture = SDL_CreateTextureFromSurface(renderer, homeSurface);
-                                    }
-                                    else
-                                    {
-                                        homeSurface = TTF_RenderText_Solid(font, getTranslatedText("paMenu.ho"), white);
-                                        homeTexture = SDL_CreateTextureFromSurface(renderer, homeSurface);
-                                    }
-                            break;
+                case SDL_MOUSEMOTION:
+                     mouse_x = pauseEvent.motion.x;
+                     mouse_y = pauseEvent.motion.y;
+                     //switch button color depending of the cursor location
+                     //case resume
+                     if ((mouse_x < resumeRect.x + resumeRect.w) && (mouse_x > resumeRect.x) &&
+                         (mouse_y < resumeRect.y + resumeRect.h) && (mouse_y > resumeRect.y)) {
+                         
+                         resume = TTF_RenderText_Solid(font, getTranslatedText("paMenu.re"), rouge);
+                         resumeTexture = SDL_CreateTextureFromSurface(renderer, resume);
+                     } else {
+                         resume = TTF_RenderText_Solid(font, getTranslatedText("paMenu.re"), white);
+                         resumeTexture = SDL_CreateTextureFromSurface(renderer, resume);
+                     }
+
+                     //case home
+                     if ((mouse_x < homeRect.x + homeRect.w) && (mouse_x > homeRect.x) &&
+                         (mouse_y < homeRect.y + homeRect.h) && (mouse_y > homeRect.y)) {
+                         
+                         homeSurface = TTF_RenderText_Solid(font, getTranslatedText("paMenu.ho"), rouge);
+                         homeTexture = SDL_CreateTextureFromSurface(renderer, homeSurface);
+                     } else {
+                         homeSurface = TTF_RenderText_Solid(font, getTranslatedText("paMenu.ho"), white);
+                          homeTexture = SDL_CreateTextureFromSurface(renderer, homeSurface);
+                     }
+                break;
 
 
-                            case SDL_MOUSEBUTTONDOWN:
-                            //resume the game if click on resume
-                             if ((pauseEvent.button.x < resumeRect.x + resumeRect.w)&&
-                                    (pauseEvent.button.x > resumeRect.x) &&
-                                    (pauseEvent.button.y < resumeRect.y + resumeRect.h)&&
-                                    (pauseEvent.button.y > resumeRect.y))
-                                        pauseRunning = SDL_FALSE;
+                case SDL_MOUSEBUTTONDOWN:
+                     //resume the game if click on resume
+                     if ((pauseEvent.button.x < resumeRect.x + resumeRect.w) && (pauseEvent.button.x > resumeRect.x) &&
+                         (pauseEvent.button.y < resumeRect.y + resumeRect.h) && (pauseEvent.button.y > resumeRect.y))
 
-                            //go to home menu if click on home
-                            if ((pauseEvent.button.x < homeRect.x + homeRect.w)&&
-                                    (pauseEvent.button.x > homeRect.x) &&
-                                    (pauseEvent.button.y < homeRect.y + homeRect.h)&&
-                                    (pauseEvent.button.y > homeRect.y))
-                                        {
-                                            pauseRunning = SDL_FALSE;
-                                            return 2;
-                                        }
-                                        
+                         pauseRunning = SDL_FALSE;
 
-                        }
+                     //go to home menu if click on home
+                     if ((pauseEvent.button.x < homeRect.x + homeRect.w) && (pauseEvent.button.x > homeRect.x) &&
+                         (pauseEvent.button.y < homeRect.y + homeRect.h) && (pauseEvent.button.y > homeRect.y)) {
+                         
+                         pauseRunning = SDL_FALSE;
+                         return 2;
+                     }
+                break;
+            }
                     
-                }
-                //clear the renderer memory
-                SDL_RenderClear(renderer);
-
-                //charge the textures into the renderer memory
-                
-                SDL_SetRenderDrawColor(renderer, *red, *green, *blue, 0);
-                SDL_RenderCopy(renderer, pauseTexture, NULL, &pauseRect);
-                SDL_RenderCopy(renderer, resumeTexture, NULL, &resumeRect);
-                SDL_RenderCopy(renderer, homeTexture, NULL, &homeRect);
-
-                //display the loaded renderer
-                SDL_RenderPresent(renderer);
         }
+        //clear the renderer memory
+        SDL_SetRenderDrawColor(renderer, *red, *green, *blue, 0);
+        SDL_RenderClear(renderer);
 
-        SDL_DestroyTexture(pauseTexture);
-        SDL_DestroyTexture(resumeTexture);
-        SDL_DestroyTexture(homeTexture);
+        //charge the textures into the renderer memory
+                
+        SDL_RenderCopy(renderer, pauseTexture, NULL, &pauseRect);
+        SDL_RenderCopy(renderer, resumeTexture, NULL, &resumeRect);
+        SDL_RenderCopy(renderer, homeTexture, NULL, &homeRect);
 
+        //display the loaded renderer
+         SDL_RenderPresent(renderer);
     }
+
+    SDL_DestroyTexture(pauseTexture);
+    SDL_DestroyTexture(resumeTexture);
+    SDL_DestroyTexture(homeTexture);
+}
